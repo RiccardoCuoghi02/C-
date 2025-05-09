85045aeee5c9c0dc6817f2b46323a4a743a7c73a
RiccardoCuoghi02 <riccardo.cuoghi@edu-its.it>
2025-05-09 15:49:34 +0000

    mnknjk# Please enter the commit message for your changes. Lines starting

---
+++ CALCOLATRICE/NomeCognome.cpp
@@ -0,0 +1,26 @@
+#include <iostream> 
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    string nome;
+    int eta;
+
+    cout<< "Inserisci il nome: ";
+    cin >> nome;
+
+    cout<< "Inserisci l'eta': ";
+    cin >> eta;
+
+    cout << "Ciao" << nome << "," << "l'eta' inserita e': "<< eta << "anni"<<endl;
+
+    cout << stamp;
+}
+
+void stamp(){
+    cout<< "-----------------------------------------"<< endl << endl;
+    cout<< "| DETTAGLI" << setw(21) << "PERSONALI" << setw(10) << "|" << endl;
+
+
+}
