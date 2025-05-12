7f0e6d34f7cb300c36c4289562d3979ce6d94bb1
RiccardoCuoghi02 <riccardo.cuoghi@edu-its.it>
2025-05-12 11:02:07 +0000

    ajeje

---
+++ CALCOLATRICE/NomeCognome02.cpp
@@ -0,0 +1,41 @@
+#include <iostream> 
+#include <iomanip>
+using namespace std;
+
+ string nome;
+ int eta;
+
+void stamp(){
+    cout<<"-----------------------------------------"<<endl;
+    cout<<"| DETTAGLI   PERSONALI                  |"<<endl;
+    cout<<"-----------------------------------------"<<endl;
+    cout<<"| "<< left << setw(10) << "Nome: "
+        << right << setw(27) << nome <<" |"<< endl;
+    
+    cout<<"| "<< left << setw(10) << "Età: "
+        << right << setw(28) << eta <<" |"<< endl;
+
+    cout<<"-----------------------------------------"<<endl;
+    string frase = "Ciao " + nome + ", hai " + to_string(eta) + " anni.";
+    cout<<"|"<< left << setw(10)<< " " 
+        << frase << right << setw(12) << " |"<<endl;
+
+    cout<<"-----------------------------------------"<<endl<<endl<<endl;
+
+}
+
+int main()
+{
+
+    cout<< "Inserisci il nome: ";
+    cin >> nome;
+
+    cout<< "Inserisci l'eta': ";
+    cin >> eta;
+
+    cout << "Ciao " << nome << "," << "l'eta' inserita e': "<< eta << " anni"<<endl;
+
+    stamp();
+
+
+}
